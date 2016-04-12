5f503196fa42f9bc928c1ce187e02b8bc33e9ae8
Jessica Tan <jatan94@gmail.com>
2016-04-11 21:04:54 -0500

    test

--- public/controllers/LoginCtrl.js
+++ public/controllers/LoginCtrl.js
@@ -1,7 +1,8 @@
 angular.module('LoginApp').controller('loginController',['$timeout', '$scope', '$http', '$location',
  '$rootScope','$window', function($timeout, $scope, $http, $location, $rootScope, $window) {
  	console.log("HELLO FROM THE LOGIN CONTROLLER");
-  	$scope.loginSubmit = function($scope){
+  	$scope.loginSubmit = functi
+  	on($scope){
 	    console.log("LOGGED IN");
 	    $window.location.href = "https://accounts.google.com/o/oauth2/v2/auth?response_type=token&client_id=403395753267-m5bosciaf32n6tmr4otncqigvfd3b2lr.apps.googleusercontent.com&redirect_uri=http://127.0.0.1:3000&scope=email%20profile";
   		console.log($location.path());
@@ -67,32 +68,3 @@ angular.module('LoginApp').controller('loginController',['$timeout', '$scope', '
 
   	
 }]);
-// First, parse the query string
-var params = {}, queryString = location.hash.substring(1),
-    regex = /([^&=]+)=([^&]*)/g, m;
-while (m = regex.exec(queryString)) {
-  params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
-}
-
-// Verify that the nonce in the response is the same as the one you sent in the
-// request.
-if (params['nonce'] !== nonce) {
-  alert('Invalid nonce.')
-} else {
-  // And send the token over to the server
-  var req = new XMLHttpRequest();
-  // consider using POST so query isn't logged
-  req.open('GET', 'https://' + window.location.host + '/catchtoken?' + queryString, true);
-  req.onreadystatechange = function (e) {
-    if (req.readyState == 4) {
-      if (req.status == 200) {
-        window.location = params['state']
-      } else if (req.status == 400) {
-        alert('There was an error processing the token.')
-      } else {
-        alert('something else other than 200 was returned')
-      }
-    }
-  };
-  req.send(null);
-}
